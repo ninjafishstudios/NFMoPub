2018a0c291e34539e09a2319b206f8a286bafca4
williamlocke <williamlocke@me.com>
2014-03-20 20:09:38 -0400

    Show banner, show interstitials set up

--- Classes/NFMoPub/NFMoPub.m
+++ Classes/NFMoPub/NFMoPub.m
@@ -7,10 +7,10 @@
 
 #import "NFMoPub.h"
 
+#import "MPInterstitialAdController.h"
 
 
-
-@interface NFMoPub(){
+@interface NFMoPub()<MPAdViewDelegate>{
 
 }
 @end
@@ -44,6 +44,51 @@
 }
 
 
+
+- (void)showInterstitial:(NSString *)label{
+    
+    self.interstitial = [MPInterstitialAdController
+                         interstitialAdControllerForAdUnitId: self.interstialAdUnitId];
+    
+    // Fetch the interstitial ad.
+    [self.interstitial loadAd];
+}
+
+
+-(UIViewController *)applicationsCurrentTopViewController{
+    UIViewController *rootViewController = [[[[UIApplication sharedApplication] delegate] window] rootViewController];
+    UIViewController *topViewController = rootViewController;
+    
+    if ([rootViewController isKindOfClass:[UITabBarController class]]){
+        topViewController = ((UITabBarController *)rootViewController).selectedViewController;
+    }else if ([rootViewController isKindOfClass:[UINavigationController class]]){
+        topViewController = ((UINavigationController *)rootViewController).topViewController;
+    }
+    return topViewController;
+}
+
+
+- (void)showBannerAd{
+    
+    self.adView = [[MPAdView alloc] initWithAdUnitId:self.bannerAdUnitId
+                                                size:MOPUB_BANNER_SIZE];
+    self.adView.delegate = self;
+    
+    UIViewController *topViewController = [self applicationsCurrentTopViewController];
+    UIView *view = topViewController.view;
+    
+    self.adView.frame = CGRectMake(0, view.bounds.size.height - MOPUB_BANNER_SIZE.height,
+                                   MOPUB_BANNER_SIZE.width, MOPUB_BANNER_SIZE.height);
+    
+    [view addSubview:self.adView];
+    [self.adView loadAd];
+    
+}
+
+#pragma mark - <MPAdViewDelegate>
+- (UIViewController *)viewControllerForPresentingModalView {
+    return [self applicationsCurrentTopViewController];
+}
 
 
 
