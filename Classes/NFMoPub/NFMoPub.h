b0a2af2ad91939848bf718d63d972dd6f67afcb3
williamlocke <williamlocke@me.com>
2014-03-21 02:18:32 -0400

    --

--- Classes/NFMoPub/NFMoPub.h
+++ Classes/NFMoPub/NFMoPub.h
@@ -9,12 +9,20 @@
 #import "MPInterstitialAdController.h"
 #import "MPAdView.h"
 
+
+typedef enum {
+    NFMoPubBannerAdPositionTop,
+    NFMoPubBannerAdPositionBottom,
+    }NFMoPubBannerAdPosition;
+
 @interface NFMoPub : NSObject
 
 @property (nonatomic, copy) NSString *interstialAdUnitId;
 @property (nonatomic, copy) NSString *bannerAdUnitId;
 @property (nonatomic, retain) MPInterstitialAdController *interstitial;
 @property (nonatomic, retain) MPAdView *adView;
+@property NFMoPubBannerAdPosition bannerAdPosition;
+@property CGRect bannerAdFrame;
 
 + (NFMoPub *)sharedInstance;
 
--- Classes/NFMoPub/NFMoPub.m
+++ Classes/NFMoPub/NFMoPub.m
@@ -69,27 +69,87 @@
 
 
 - (void)showBannerAd{
+    [self.adView removeFromSuperview];
+    
+    CGSize bannerSize;
+    if([[UIDevice currentDevice] userInterfaceIdiom] == UIUserInterfaceIdiomPad){
+        bannerSize = CGSizeMake(728, 90);
+    }else{
+        bannerSize = CGSizeMake(320, 50);
+    }
     
     self.adView = [[MPAdView alloc] initWithAdUnitId:self.bannerAdUnitId
-                                                size:MOPUB_BANNER_SIZE];
+                                                size:bannerSize];
     self.adView.delegate = self;
     
     UIViewController *topViewController = [self applicationsCurrentTopViewController];
     UIView *view = topViewController.view;
     
-    self.adView.frame = CGRectMake(0, view.bounds.size.height - MOPUB_BANNER_SIZE.height,
-                                   MOPUB_BANNER_SIZE.width, MOPUB_BANNER_SIZE.height);
+
     
+    if(self.bannerAdPosition == NFMoPubBannerAdPositionBottom){
+        self.bannerAdFrame = CGRectMake(0, view.bounds.size.height - bannerSize.height,
+                                        bannerSize.width, bannerSize.height);
+        self.adView.frame = CGRectOffset(self.bannerAdFrame, 0, self.bannerAdFrame.size.height);
+        
+        
+    }else if(self.bannerAdPosition == NFMoPubBannerAdPositionTop){
+        self.bannerAdFrame = CGRectMake(0, 0,
+                                        bannerSize.width, bannerSize.height);
+        self.adView.frame = CGRectOffset(self.bannerAdFrame, 0, -self.bannerAdFrame.size.height);
+    }
     [view addSubview:self.adView];
     [self.adView loadAd];
-    
 }
+
 
 #pragma mark - <MPAdViewDelegate>
 - (UIViewController *)viewControllerForPresentingModalView {
     return [self applicationsCurrentTopViewController];
 }
 
+- (void)adViewDidLoadAd:(MPAdView *)view
+{
+    NSLog(@"ad view did load");
+    [UIView animateWithDuration:0.7 animations:^{
+        self.adView.frame = self.bannerAdFrame;
+    }completion:^(BOOL finished){
+    }];
+    
+    [self makeSpaceForBannerAd];
+}
+
+
+-(void)makeSpaceForBannerAd{
+    
+    NSMutableArray *viewsToMove = [[NSMutableArray alloc] init];
+    UIViewController *topViewController = [self applicationsCurrentTopViewController];
+    UIView *view = topViewController.view;
+    
+    for(UIView *subview in view.subviews){
+        if(abs(subview.frame.origin.y - self.adView.frame.origin.y) < self.adView.frame.size.height * 0.8){
+            if(subview.frame.size.height < view.frame.size.height * 0.4 && subview != self.adView){
+                [viewsToMove addObject:subview];
+            }
+        }
+    }
+    
+    int directionToMoveViews = 1;
+    
+    if(self.adView.frame.origin.y > view.frame.size.height * 0.5){
+        directionToMoveViews = -1;
+    }
+    
+    
+    [UIView animateWithDuration:0.7 animations:^{
+        for (UIView *subview in viewsToMove){
+            subview.frame = CGRectOffset(subview.frame, 0, self.adView.frame.size.height * directionToMoveViews);
+        }
+    }completion:nil];
+
+    
+    
+}
 
 
 @end
